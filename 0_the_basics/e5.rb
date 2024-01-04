f139bb2796a6c1542af1324ed6147421dc0b04ad
Michelangelo <m@michelangelo.cods>
2024-01-04 22:51:33 +0700

    () are optional? What!?

--- 0_the_basics/e5.rb
+++ 0_the_basics/e5.rb
@@ -1,4 +1,4 @@
-def calculate_factorial(number)
+def calculate_factorial number
   total = number
   while number - 1 > 0
     total *= number - 1
@@ -7,7 +7,7 @@ def calculate_factorial(number)
   total
 end
 
-puts calculate_factorial(5)
-puts calculate_factorial(6)
-puts calculate_factorial(7)
-puts calculate_factorial(8)
+puts calculate_factorial 5
+puts calculate_factorial 6
+puts calculate_factorial 7
+puts calculate_factorial 8
