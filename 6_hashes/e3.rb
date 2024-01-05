046f0b23ead490b1ef271f0fbe57ed94feebfb40
Michelangelo <m@michelangelo.cods>
2024-01-05 22:24:00 +0700

    finished 6_hashes

---
+++ 6_hashes/e3.rb
@@ -0,0 +1,10 @@
+family = {uncles: ["bob", "joe", "steve"],
+          sisters: ["jane", "jill", "beth"],
+          brothers: ["frank", "rob", "david"],
+          aunts: ["mary", "sally", "susan"]}
+
+family.each_key { |k| puts k }
+
+family.each_value { |v| p v }
+
+family.each { |k, v| puts "#{k}, #{v}" }
